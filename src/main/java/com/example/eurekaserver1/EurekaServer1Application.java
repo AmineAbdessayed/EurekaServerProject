36fffff5ede52ef6278ec33e1df32f7dfa2591d1
Amine <mohamedamine.abdessayed@esprit.tn>
2023-11-19 18:30:01 +0100

    Y

---
+++ src/main/java/com/example/eurekaserver1/EurekaServer1Application.java
@@ -0,0 +1,15 @@
+package com.example.eurekaserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer1Application.class, args);
+    }
+
+}
